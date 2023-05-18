fd270bdd9cbaa14c7bb3e939b7aa4498a1f8b77f
Akshay Ahuja <akshay.ahuja2187@gmail.com>
2023-05-18 15:42:54 +0000

    fixing

--- init.tf
+++ init.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  subscription_id            = ${{ secrets.AZURE_SUBSCRIPTION_ID }}
-  tenant_id                  = ${{ secrets.AZURE_TENANT_ID }}
-  client_id                  = ${{ secrets.AZURE_CLIENT_ID }}
-  client_secret              = ${{ secrets.AZURE_CLIENT_SECRET }}
   skip_provider_registration = true
   features {}
 }
