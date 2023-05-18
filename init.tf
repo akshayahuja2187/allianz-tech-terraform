224350ac47504f5e8c61f16bbcbe0ac5db875c83
Akshay Ahuja <akshay.ahuja2187@gmail.com>
2023-05-18 14:11:16 +0000

    added tf state file changes and gitignore

--- init.tf
+++ init.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "cloud-shell-storage-southeastasia"
+      storage_account_name = "cs110032001fc8b8730"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
 provider "azurerm" {
   subscription_id            = var.subscription_id
   client_id                  = var.client_id
